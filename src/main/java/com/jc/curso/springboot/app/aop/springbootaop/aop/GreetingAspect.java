ad59432812675fb09f660243d12db50964539dcf
jcmartinez <jcmartinezcu@gmail.com>
2024-08-16 18:20:53 +0200

    creando componente aspect

---
+++ src/main/java/com/jc/curso/springboot/app/aop/springbootaop/aop/GreetingAspect.java
@@ -0,0 +1,27 @@
+package com.jc.curso.springboot.app.aop.springbootaop.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class GreetingAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(* com.jc.curso.springboot.app.aop.springbootaop.service.GreetingService.sayHello(..))")
+    public void loggerBefore(JoinPoint joinPoint){
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("Antes: " + method + " con los argumentos " + args);
+    }
+
+}
